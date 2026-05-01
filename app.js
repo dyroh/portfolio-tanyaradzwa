12d272c50e994be751ebc99e9eccb2d18a907ba6
dyroh <muchabaiwatanyaradzwa034@gmail.com>
2026-05-01 12:10:54 -0700

    New database for deployment

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ app.get("/", (req, res) => {
 
 /* ============================================ DATABASE CONNECTION =================================== */
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "portfolio_db"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 db.connect(err => {
